a462878fbe7f640fd7fdd03751cb5af05918228d
Odaine Codner <odaine.codner@gmail.com>
2020-06-03 08:44:08 +0100

    unit testing

--- JS-10 - Unit Tests in JavaScript/js/calculator/calculator.js
+++ JS-10 - Unit Tests in JavaScript/js/calculator/calculator.js
@@ -13,4 +13,4 @@ function multiply(a, b){
   return a * b;
 }
 
-module.exports = {add: add, subtract: subtract};
+module.exports = {add: add, subtract: subtract, divide: divide, multiply: multiply};
--- JS-10 - Unit Tests in JavaScript/js/calculator/calculator.test.js
+++ JS-10 - Unit Tests in JavaScript/js/calculator/calculator.test.js
@@ -12,6 +12,24 @@ test('subtract 5 - 4 to equal 1', () => {
   expect(calculator.subtract(5, 4)).toBe(1);
 });
 
+
+
 test('subtract -5 - -10 to equal 5', () => {
   expect(calculator.subtract(-5, -10)).toBe(5);
+});
+
+test('divides 10 by 2 to equal 5', () => {
+  expect(calculator.divide(10, 2)).toBe(5);
+});
+
+test('divides 30 by 5 to equal 6', () => {
+  expect(calculator.divide(30, 5)).toBe(6);
+});
+
+test('multiplies 3 by 4 to equal 12', () => {
+  expect(calculator.multiply(3, 4)).toBe(12);
+});
+
+test('multiplies 7 by 3 to equal 21', () => {
+  expect(calculator.multiply(7, 3)).toBe(21);
 });
